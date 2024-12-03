a0562b5bd37bf39ae7ec10418b7604ade31e39dc
rr3khan <riyad@LAPTOP-C1G6NBR5>
2024-12-03 01:34:52 -0400

    Add initial test

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_star_map_distances() {
+        let list_1 = vec![3, 4, 2, 1, 3, 3];
+        let list_2 = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(star_map_distances(), 11);
+    }
+}
