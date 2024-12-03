e12b29c9ff024de49d98fecdc90adad6505fca9c
rr3khan <riyad@LAPTOP-C1G6NBR5>
2024-12-03 02:32:29 -0400

    Day 1 Part 2 Similarity Score

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,41 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use day_1::{similarity_score, star_map_distances};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Read the data from the input file
+    let input_file = File::open("input.txt").expect("Unable to open file");
+    let reader = BufReader::new(input_file);
+
+    let mut list_1 = Vec::new();
+    let mut list_2 = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        let mut parts = line.split_whitespace();
+
+        if let (Some(part1), Some(part2)) = (parts.next(), parts.next()) {
+            let num1: i32 = part1.parse()?;
+            let num2: i32 = part2.parse()?;
+
+            list_1.push(num1);
+            list_2.push(num2);
+        }
+    }
+
+    star_map_distances(&mut list_1, &mut list_2);
+
+    similarity_score(&mut list_1, &mut list_2);
+
+    Ok(())
 }
 
 #[cfg(test)]
-
-/// Calculates the "distance" between two lists of integers.
-///
-/// The function sorts both lists, aligns them by index, and computes the
-/// absolute difference between corresponding elements. These differences
-/// are summed to produce the final "star map distance."
-///
-/// # Arguments
-///
-/// * `list_1`: i8 - First list of locations.
-/// * `list_2`: i8 - Second list of locations.
-///
-/// # Returns
-///
-/// An `i8` representing the total "star map distance."
-///
-fn star_map_distances(list_1: &mut Vec<i8>, list_2: &mut Vec<i8>) -> i8 {
-    // Sort both lists in numerical order to get smallest
-    // nums of both aligned w.r.t. index
-    list_1.sort();
-    list_2.sort();
-
-    let mut star_distance = 0;
-
-    for (index, num) in list_1.iter().enumerate() {
-        star_distance = star_distance + (num - list_2[index]).abs();
-    }
-
-    println!("list 1 {:?}", list_1);
-    println!("list 2 {:?}", list_2);
-    println!("Star Dist: {:?}", star_distance);
-    return star_distance;
-}
 
 mod tests {
     use super::*;
@@ -45,5 +41,12 @@ mod tests {
         let mut list_1 = vec![3, 4, 2, 1, 3, 3];
         let mut list_2 = vec![4, 3, 5, 3, 9, 3];
         assert_eq!(star_map_distances(&mut list_1, &mut list_2), 11);
+    }
+
+    #[test]
+    fn test_similarity_scores() {
+        let mut list_1 = vec![3, 4, 2, 1, 3, 3];
+        let mut list_2 = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(similarity_score(&mut list_1, &mut list_2), 31);
     }
 }
